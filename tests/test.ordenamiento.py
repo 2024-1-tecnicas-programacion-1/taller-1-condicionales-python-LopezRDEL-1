ca068585640290afffacd8ca1c720156c4cd20fd
LopezDEL <diegoestebanlopezrico3@gmail.com>
2024-03-12 18:00:21 -0500

    ORDENAMIENTO_TEST

--- tests/test.ordenamiento.py
+++ tests/test.ordenamiento.py
@@ -12,12 +12,27 @@ sys.path.append(str(root_path))
 from src.ordenamiento import evaluar
 
 class TestOrdenamiento(unittest.TestCase):
-    def testNo(self):
+     def testNo(self):
         valor_esperado = "0 1 6 7"
         valor_actual = evaluar(7, 0, 6, 1)
         self.assertEqual(valor_esperado, valor_actual)
     
-    # TODO: Agrega tus otros casos de prueba aquí
+       def testAscendente(self):
+        valor_esperado = "1 2 3 4"
+        valor_actual = evaluar(1, 2, 3, 4)
+        self.assertEqual(valor_esperado, valor_actual)
+    
+        def testDescendente(self):
+        valor_esperado = "4 3 2 1"
+        valor_actual = evaluar(4, 3, 2, 1)
+        self.assertEqual(valor_esperado, valor_actual)
+    
+         def testIguales(self):
+        valor_esperado = "3 3 3 3"
+        valor_actual = evaluar(3, 3, 3, 3)
+        self.assertEqual(valor_esperado, valor_actual)
+
+
     
 
 if __name__ == '__main__':
